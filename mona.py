e890b1f0a6187f2f0c675603e25ae8f4780e2090
monamolham <monamolham@gmail.com>
2017-10-21 16:01:35 +0200

    changed mona.py

---
+++ mona.py
@@ -0,0 +1,23 @@
+"""
+1-the data type:
+integer
+real
+list
+dictionary
+tulpe
+complex
+logic
+functions
+classes -> defiended types
+2-condition
+3-loop
+
+"""
+x=5
+y= 5.
+z=x+y
+t= x>6
+##k= x>5
+print x, y, z, t
+print type(x), type(y), type(k), k
+print x/2, y/2, z/2, int(z/2), round(z/2)
